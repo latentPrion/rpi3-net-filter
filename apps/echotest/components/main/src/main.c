a4e464a554798b5212eba99a9fa8e3095b1bfccc
Kofi Doku Atuah <kofidoku.atuah@data61.csiro.au>
2018-10-24 19:02:31 +0200

    Add hierarchically constructed IP filtering application

---
+++ apps/echotest/components/main/src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void map_hello(int n)
+{
+	printf("Map hello %d\n", n);
+}
+
+void map_goodbye(int n)
+{
+	printf("Map hello %d\n", n);
+}
+
+int run(void)
+{
+	printf("HEllo from main.\n");
+	return 0;
+}
---
+++ components/rpi3-ip-stack/src/main.c
@@ -0,0 +1,12 @@
+int mgmt_api_configure(int n)
+{
+	return 0;
+}
+
+int sockets_api_socket(int a) { return 0; }
+int sockets_api_bind(int a) { return 0; }
+int sockets_api_listen(int a) { return 0; }
+int sockets_api_connect(int a) { return 0; }
+int sockets_api_accept(int a) { return 0; }
+int sockets_api_send(int a) { return 0; }
+int sockets_api_recv(int a) { return 0; }
